be3e7c09be739cb2fa150da734ab238fd0eb92f2
V1cioxD <matias.ortega2004b@gmail.com>
2025-05-28 23:36:05 -0400

    scss y botones
    sex

--- src/app/contacto/privacidad-terminos/privacidad-terminos.page.ts
+++ src/app/contacto/privacidad-terminos/privacidad-terminos.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-privacidad-terminos',
@@ -9,13 +9,13 @@ import { Location } from '@angular/common';
 export class PrivacidadTerminosPage implements OnInit {
 
 
-  constructor(private location: Location) { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
   goBack() {
-    this.location.back();
+    this.router.navigate(['/informacion']);
   }
 
 }
--- src/app/contacto/quienes-somos/quienes-somos.page.ts
+++ src/app/contacto/quienes-somos/quienes-somos.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-quienes-somos',
@@ -8,12 +8,14 @@ import { Location } from '@angular/common';
 })
 export class QuienesSomosPage implements OnInit {
 
-  constructor(private location: Location) { }
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
   goBack() {
-    this.location.back();
+    this.router.navigate(['/informacion']);
   }
+
 }
